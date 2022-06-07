5f70ed6eb9d05a248d648ce858b9cae10bf9dd65
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-06-07 20:35:08 +0900

    first commit

--- frontend/src/book_app/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-    proxy: 'http://0.0.0.0:8000'
-  }
-})
