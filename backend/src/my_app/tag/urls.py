11ded2611a0bd48b84d2b5faec40809151edb71c
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-06-08 21:29:16 +0900

    add: タグAPIの実装

---
+++ backend/src/my_app/tag/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TagListCreateAPIView, TagRetrieveUpdateDestroyAPIView
+
+urlpatterns = [
+    path('index/', TagListCreateAPIView.as_view(), name="tag_list_create_api"),
+    path('<int:pk>/', TagRetrieveUpdateDestroyAPIView.as_view()),
+]
--- backend/src/my_app/tag/views.py
+++ backend/src/my_app/tag/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import TagSerializer
+from .models import Tag
 # Create your views here.
+
+
+class TagListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class TagRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
