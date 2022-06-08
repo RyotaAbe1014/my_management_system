363327700f3d9f0dc2bfde15e3dc25370e15ba3d
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-06-08 23:37:34 +0900

    frontend追加 axiosでget確認まで

---
+++ frontend/src/my_app/src/views/TopPageView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <hello-world />
+    <button @click="get">ssss</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+export default {
+  name: "TopPage",
+
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    async get() {
+      const data = await this.axios
+        .get("http://0.0.0.0:8000/api/tag/index/")
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => {
+          console.log("エラー", e);
+        });
+        console.log(data);
+    },
+  },
+};
+</script>
