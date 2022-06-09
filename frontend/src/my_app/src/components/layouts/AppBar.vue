fd094023c14c80a656fdf4053a6b3f24c9d1895e
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-06-09 21:40:57 +0900

    add:トップページの作成

---
+++ frontend/src/my_app/src/components/layouts/AppBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-5">
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-calendar-edit</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>日報を書く</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-text-box-plus</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>目標を書く</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-tag</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>タグを作成する</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="deep-purple accent-4" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>自己管理アプリ</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+};
+</script>
--- frontend/src/my_app/src/views/TopPageView.vue
+++ frontend/src/my_app/src/views/TopPageView.vue
@@ -1,17 +1,128 @@
 <template>
-  <div>
-    <hello-world />
-    <button @click="get">ssss</button>
-  </div>
+  <v-app id="inspire">
+    <AppBar />
+    <v-main class="grey lighten-2">
+      <v-container>
+        <h1>dashboard</h1>
+        <v-row>
+          <template>
+            <v-col class="mt-2" cols="12">
+              <strong>日報</strong>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">日報作成</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">日報一覧</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">日報検索</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </template>
+          <template>
+            <v-col class="mt-2" cols="12">
+              <strong>目標管理</strong>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">目標作成</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">未達成一覧</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">達成済一覧</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </template>
+          <template>
+            <v-col class="mt-2" cols="12">
+              <strong>タグ管理</strong>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">タグ作成</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">タグ一覧</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="2">
+              <v-card class="mx-auto" max-width="344" height="150">
+                <v-card-text>
+                  <p class="text-h5 text--primary">タグ検索</p>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="mx-auto" outlined color="primary"> GO </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import AppBar from "../components/layouts/AppBar.vue";
 export default {
   name: "TopPage",
 
   components: {
-    HelloWorld,
+    AppBar,
   },
   methods: {
     async get() {
@@ -23,7 +134,7 @@ export default {
         .catch((e) => {
           console.log("エラー", e);
         });
-        console.log(data);
+      console.log(data);
     },
   },
 };
