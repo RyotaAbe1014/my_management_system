04b8f494694d84d7b9b5f802be18f134e492cc07
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-06-11 14:21:20 +0900

    ログアウト機能を追加

--- frontend/src/my_app/src/components/layouts/AppBar.vue
+++ frontend/src/my_app/src/components/layouts/AppBar.vue
@@ -2,7 +2,7 @@
   <div>
     <v-navigation-drawer v-model="drawer" app>
       <v-sheet color="grey lighten-4" class="pa-5">
-        <div>john@vuetifyjs.com</div>
+        <div>{{ auth.name }}</div>
       </v-sheet>
       <v-divider></v-divider>
       <v-list>
@@ -22,20 +22,28 @@
             <v-list-item-title>目標を書く</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-          <v-list-item to="/tag/create">
-            <v-list-item-icon>
-              <v-icon>mdi-tag</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>タグを作成する</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+        <v-list-item to="/tag/create">
+          <v-list-item-icon>
+            <v-icon>mdi-tag</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>タグを作成する</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
         <v-list-item to="/">
           <v-list-item-icon>
             <v-icon>mdi-home</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>ホーム</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link @click="logout">
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>ログアウト</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -53,7 +61,17 @@ export default {
   data() {
     return {
       drawer: true,
+      auth: [],
     };
+  },
+  mounted() {
+    this.auth = JSON.parse(sessionStorage.getItem("user"));
+  },
+  methods: {
+    logout() {
+      sessionStorage.removeItem("user");
+      this.$router.push("/login");
+    },
   },
 };
 </script>
